170ee66432b037a363e9b2360dd01a014c9bb6e7
Mauro Candotti <cmduilio@gmail.com>
2020-12-04 21:09:41 -0300

    first commit

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+    return(
+            <nav>
+                <ul>
+                     <li><a href="#"> Register</a></li>
+                </ul>
+            </nav>
+    );
+}
+
+export default Navbar;
