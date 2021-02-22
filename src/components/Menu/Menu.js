c9f3b7d9e61e36463f32a6a1ac90b44d9e23075b
Mauro Candotti <cmduilio@gmail.com>
2021-02-21 22:15:28 -0300

    menu and navbar are now responsive

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,21 +2,21 @@ import React from 'react';
 import './Menu.css';
 import MenuItem from "../MenuItem/MenuItem";
 
-function Menu({home, menu, style, itemStyle}) {
+function Menu({home, menu, style, itemStyle, onClick}) {
     return (
-        <ul className={"menu"} style={style}>
-            <li key={home.text} className={"item"} style={{marginRight: "auto"}}>
+        <div className={"menu"} style={style} onClick={onClick}>
+            <div key={home.text} className={"item"} style={{marginRight: "auto"}}>
                 <MenuItem url={"#"} text={home.text} style={itemStyle}/>
-            </li>
+            </div>
 
             {menu.map(data => {
                 return (
-                    <li key={data.text} className={"item"}>
+                    <div key={data.text} className={"item"}>
                         <MenuItem style={itemStyle} url={data.url} text={data.text}/>
-                    </li>
+                    </div>
                 )
             })}
-        </ul>
+        </div>
     )
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import './Navbar.css';
 import Menu from "../Menu/Menu";
 
 function Navbar({home, menu, sticky}) {
+    const checkRef = useRef(null);
+
     return(
-            <nav className={"navbar"} style={sticky ? {position: "sticky"}: {}}>
-                <Menu home={home} menu={menu}/>
-            </nav>
+        <div className={"navbar"} onBlur={() => {
+            console.log("pepe");
+        }}
+             style={sticky ? {position: "sticky"}: {}}>
+            <label htmlFor={"toogle"} className={"hamburguer item"}>
+                &#9776;
+            </label>
+            <input type={"checkbox"} id={"toogle"} ref={checkRef}/>
+            <Menu home={home} menu={menu} onClick={() => {
+                checkRef.current.checked = false;
+            }}/>
+        </div>
     );
 }
 
